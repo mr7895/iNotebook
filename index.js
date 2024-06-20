4eea366b6a5cc947f3fa235c1490556eeb7164eb
mr7895 <148631790+mr7895@users.noreply.github.com>
2024-06-20 10:56:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
-const connectToMongo = require('./db');
-const express = require('express')
-var cors = require('cors') 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'; 
 
-connectToMongo();
-const app = express()
-const port = 5000
-
-app.use(cors())
-app.use(express.json())
-
-// Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/notes', require('./routes/notes'))
-
-
-app.listen(port, () => {
-  console.log(`iNotebook backend listening at http://localhost:${port}`)
-})
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+ 
